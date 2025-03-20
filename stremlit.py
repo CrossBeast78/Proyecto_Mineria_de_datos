24d3c773f6d9c6dd25e8fe613cbff6bfb103b545
Luis Guillermo Rivera <memoriverastephens@gmail.com>
2025-03-19 18:24:45 -0600

    agregando la tabla de recomendaciones

--- stremlit.py
+++ stremlit.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler, KBinsDiscretizer
+import heapq
 
 # Cargar datos
 file_path = "datos/Dados_totais.csv"
@@ -10,6 +11,9 @@ df = pd.read_csv(file_path)
 
 # --- Menú de configuración ---
 st.sidebar.title("Configuración de Datos")
+
+st.sidebar.subheader("Seleccionar cancion")
+selected_song = st.sidebar.selectbox("Selecciona una cancion:", df['artists_song'].unique())
 
 # Selección de columnas a eliminar
 all_columns = list(df.columns)
@@ -59,18 +63,18 @@ st.write(df_cleaned.head(10))
 # --- Cálculo de Gender Index ---
 st.subheader("Tabla para sacar el índice de género")
 columnas_ = ["id", "danceability", "valence", "liveness", "instrumentalness", "loudness"]
-gender_index_df = df[columnas_]
-st.write(gender_index_df.head(10))
+genre_index_df = df[columnas_]
+st.write(genre_index_df.head(10))
 
 # Normalización de datos
 st.subheader("Normalización de los datos")
 scaler = MinMaxScaler()
 columns_to_normalize = ['danceability', 'valence', 'liveness', 'instrumentalness']
-gender_index_df[columns_to_normalize] = scaler.fit_transform(gender_index_df[columns_to_normalize])
+genre_index_df[columns_to_normalize] = scaler.fit_transform(genre_index_df[columns_to_normalize])
 
 # Normalizar loudness por separado para valores entre -1 y 0
 loudness_scaler = MinMaxScaler(feature_range=(-1, 0))
-gender_index_df['loudness'] = loudness_scaler.fit_transform(gender_index_df[['loudness']])
+genre_index_df['loudness'] = loudness_scaler.fit_transform(genre_index_df[['loudness']])
 
 # Función para calcular Gender Index
 def get_gender_index(danceability, valence, liveness, instrumentalness, loudness):
@@ -97,32 +101,32 @@ class GenderIndexCalculator:
         return get_gender_index(self.danceability, self.valence, self.liveness, self.instrumentalness, self.loudness)
 
 # Calcular Gender Index
-arr_gender_index = []
-for i in range(gender_index_df.shape[0]):
-    gender_index = GenderIndexCalculator(gender_index_df.iloc[i]['id'], 
-                                       gender_index_df.iloc[i]['danceability'],
-                                       gender_index_df.iloc[i]['valence'], 
-                                       gender_index_df.iloc[i]['liveness'],
-                                       gender_index_df.iloc[i]['instrumentalness'],
-                                       gender_index_df.iloc[i]['loudness'])
-    arr_gender_index.append(gender_index.get_gender_index())
+arr_genre_index = []
+for i in range(genre_index_df.shape[0]):
+    genre_index = GenderIndexCalculator(genre_index_df.iloc[i]['id'], 
+                                       genre_index_df.iloc[i]['danceability'],
+                                       genre_index_df.iloc[i]['valence'], 
+                                       genre_index_df.iloc[i]['liveness'],
+                                       genre_index_df.iloc[i]['instrumentalness'],
+                                       genre_index_df.iloc[i]['loudness'])
+    arr_genre_index.append(genre_index.get_gender_index())
 
-gender_index_df['gender_index'] = arr_gender_index
+genre_index_df['genre_index'] = arr_genre_index
 
 # Normalizar Gender Index
 scaler = MinMaxScaler(feature_range=(min_range, max_range))
-gender_index_df['gender_index'] = scaler.fit_transform(gender_index_df[['gender_index']])
+genre_index_df['genre_index'] = scaler.fit_transform(genre_index_df[['genre_index']])
 
 # Mostrar tabla con los valores normalizados
 st.subheader("Tabla con los valores normalizados")
-st.write(gender_index_df.head(10))
+st.write(genre_index_df.head(10))
 
 # Crear un DataFrame con solo 'id' y 'gender_index'
-gender_index_simple = gender_index_df[['id', 'gender_index']]
+genre_index_simple = genre_index_df[['id', 'genre_index']]
 
 # Unir los datos originales con los datos de género
 st.subheader("Tabla combinada de datos originales y el índice de género")
-resultado = pd.merge(df_cleaned, gender_index_simple, on='id')
+resultado = pd.merge(df_cleaned, genre_index_simple, on='id')
 st.write(resultado.head(10))
 
 # Clase para normalizar canciones
@@ -153,8 +157,8 @@ class Cancion_normalizada:
 # Normalización de resultado
 scaler = MinMaxScaler(feature_range=(min_range, max_range))
 resultado_normalizado = resultado.copy()
-resultado_normalizado = resultado_normalizado.drop(columns=['name', 'artists', 'year', 'explicit', 'artists_song'])
-resultado_normalizado.set_index('id', inplace=True)
+resultado_normalizado = resultado_normalizado.drop(columns=['name', 'artists', 'year', 'explicit', 'id'])
+resultado_normalizado.set_index('artists_song', inplace=True)
 resultado_normalizado[resultado_normalizado.columns] = scaler.fit_transform(resultado_normalizado[resultado_normalizado.columns])
 
 st.subheader("Tabla con los datos normalizados de canciones")
@@ -164,11 +168,70 @@ if show_histograms:
     fig, axes = plt.subplots(2, 2, figsize=(12, 8))
     for i, col in enumerate(columns_to_normalize):
         ax = axes[i // 2, i % 2]
-        sns.histplot(gender_index_df[col], bins=20, kde=True, ax=ax)
+        sns.histplot(genre_index_df[col], bins=20, kde=True, ax=ax)
         ax.set_title(f"Distribución de {col}")
         ax.set_xlabel(col)
         ax.set_ylabel("Frecuencia")
     plt.tight_layout()
     st.pyplot(fig)
 
+def calcular_similitud(resultado_normalizado, artists_song):
+    if artists_song not in resultado_normalizado.index:
+        raise KeyError(f"cancion '{artists_song}' no encontrado en el DataFrame.")
+    target_row = resultado_normalizado.loc[artists_song]
+
+    target = Cancion_normalizada(
+        target_row['valence'],
+        target_row['danceability'],
+        target_row['energy'],
+        artists_song,
+        target_row['popularity'],
+        target_row['tempo'],
+        target_row['genre_index']
+    )
+
+    similitudes = {}
+
+    for other_artists_song, row in resultado_normalizado.iterrows():
+        other = Cancion_normalizada(
+            row['valence'],
+            row['danceability'],
+            row['energy'],
+            other_artists_song,
+            row['popularity'],
+            row['tempo'],
+            row['genre_index']
+        )
+        similitudes[other_artists_song] = target.compare_song_index(other)
+
+    return similitudes
+
+similitudes = calcular_similitud(resultado_normalizado, selected_song)
+st.header(f"Similitudes respecto a '{selected_song}'")
+
+
+# Obtener los 5 elementos con mayor similitud sin ordenar todo el diccionario
+top5_similitudes = heapq.nlargest(6, similitudes.items(), key=lambda item: item[1])
+
+# Crear un DataFrame con las canciones y sus índices de similitud
+top5_df = pd.DataFrame(top5_similitudes, columns=["Canción", "Similitud"])
+st.write("Tabla con el índice de similitud:")
+st.table(top5_df)
+
+# Separar las etiquetas (canciones) y los valores de similitud para graficar
+top5_canciones, top5_valores = zip(*top5_similitudes)
+
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.bar(top5_canciones, top5_valores)
+ax.set_ylim(0, 1)
+ax.set_ylabel('Similitud')
+ax.set_xlabel('Canción')
+ax.set_title(f"Top 5 canciones más similares a '{selected_song}'")
+
+# Escapar '$' en las etiquetas si es necesario
+escaped_labels = [cancion.replace('$', r'\$') for cancion in top5_canciones]
+ax.set_xticklabels(escaped_labels, rotation=90, fontsize=8)
+
+plt.tight_layout()
+st.pyplot(fig)
 st.write("Transformaciones aplicadas con éxito. Explora los gráficos para visualizar los cambios en los datos.")
